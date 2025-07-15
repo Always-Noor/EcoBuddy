7eb656eab21af069b334d890a2200b32d496275c
Faith Sfetcu <faithsfetcu7@gmail.com>
2025-07-15 11:08:13 -0400

    Initial Commit

---
+++ EcoBuddy/EcoBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoBuddyApp.swift
+//  EcoBuddy
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct EcoBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
