a54285f1e5a6e24ddea42eec291eb79af3b165c6
gianne <giannguyengervacio@gmail.com>
2025-07-15 19:09:07 +0200

    added a spacer

--- EcoBuddy/NewTripView.swift
+++ EcoBuddy/NewTripView.swift
@@ -1,17 +1,16 @@
-//
-//  NewTripView.swift
-//  EcoBuddy
-//
-//  Created by Orvs on 7/15/25.
-//
-
 import SwiftUI
 
 struct NewTripView: View {
     var body: some View {
         VStack {
-            Text("New Trip")
+            Text("New trip:")
+                .font(.title)
+                .fontWeight(.bold)
             TextField("Name", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding()
         }
     }
 }
--- EcoBuddy/TripEditor.swift
+++ EcoBuddy/TripEditor.swift
@@ -24,6 +24,7 @@ struct TripEditor: View {
                 }
             }
             .padding()
+            Spacer()
         }
     }
 }
