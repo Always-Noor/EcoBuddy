9d8d548832e45dc9bead4a9335c24335f8bd67b6
Anisa Nurani <nurania152@gmail.com>
2025-07-17 23:24:27 -0400

    i added a delete function so the user can delete any items

--- EcoBuddy/TripEditor.swift
+++ EcoBuddy/TripEditor.swift
@@ -3,57 +3,66 @@ import SwiftData
 
 
 struct TripEditor: View {
-  let availableItems: [String: Int] = [
-    "Shoes": 14,
-    "Dress": 22,
-    "Jeans": 17,
-    "Purse": 14,
-    "T-Shirt": 7,
-    "Long Sleeve Shirt": 10
-  ]
-  @State private var selectedItem = "Shoes"
-  @State private var items: [InventoryItem] = []
-  var body: some View {
-      NavigationStack {
-          
-          ZStack {
-              Color(red: 0.748, green: 0.854, blue: 0.857)
-                  .ignoresSafeArea()
-              
-              
-              VStack() {
-                  List(items) { item in
-                      HStack {
-                          Text("+")
-                          Text(item.name)
-                          Spacer()
-                          Text("\(item.carbonFootprint)")
-                      }
-                      
-                  }
-                  .background(Color(red: 0.708, green: 0.819, blue: 0.459))
-                  .shadow(radius: 5)
-                  .listStyle(PlainListStyle())
-                  Picker("Select an item", selection: $selectedItem) {
-                      ForEach(availableItems.keys.sorted(), id: \.self) { item in
-                          Text(item)
-                      }
-                  }
-                  .pickerStyle(MenuPickerStyle())
-                  .padding(.horizontal)
-                  Button("Save") {
-                      if let footprint = availableItems[selectedItem] {
-                          items.append(InventoryItem(name: selectedItem, carbonFootprint: footprint))
-                      }
-                      
-                  }
-              } //closes VStack
-              .navigationTitle("Items & Footprint")
-              .padding()
-          } //closes Navigation
-      }
-  } //closes Body
+    let availableItems: [String: Int] = [
+        "Shoes": 14,
+        "Dress": 22,
+        "Jeans": 17,
+        "Purse": 14,
+        "T-Shirt": 7,
+        "Long Sleeve Shirt": 10
+    ]
+    @State private var selectedItem = "Shoes"
+    
+    @State private var items: [(String, Int)] = []
+    
+    var body: some View {
+        NavigationStack {
+            
+            ZStack {
+                Color(red: 0.748, green: 0.854, blue: 0.857)
+                    .ignoresSafeArea()
+                
+                
+                VStack {
+                    List {
+                        ForEach(Array(items.enumerated()), id: \.offset) { index, item in
+                            HStack {
+                                Text("+")
+                                Text(item.0)
+                                Spacer()
+                                Text("\(item.1)")
+                            }
+                        }
+                        .onDelete(perform: deleteItem)
+                    }
+                    .background(Color(red: 0.708, green: 0.819, blue: 0.459))
+                    .shadow(radius: 5)
+                    .listStyle(PlainListStyle())
+                    Picker("Select an item", selection: $selectedItem) {
+                        ForEach(availableItems.keys.sorted(), id: \.self) { item in
+                            Text(item)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                    .padding(.horizontal)
+                    Button("Save") {
+                        if let footprint = availableItems[selectedItem] {
+                            items.append((selectedItem, footprint))
+                        }
+                        
+                    }
+                } //closes VStack
+                .navigationTitle("Items & Footprint")
+                .padding()
+            }
+        }
+    } //closes Body
+    func deleteItem(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+    }
 }
-#Preview {
-  TripEditor()
-}
+
+        #Preview {
+            TripEditor()
+        }
+
